a568437b3443fafbbb21d81384c8585d17cd47cc
GabrielGPena793 <kbeca.gabriel793@gmail.com>
2023-06-27 01:52:29 -0300

    complete example config biometric

--- app/src/main/java/com/example/biometric/MainActivity.kt
+++ app/src/main/java/com/example/biometric/MainActivity.kt
@@ -1,14 +1,38 @@
 package com.example.biometric
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.biometric.BiometricPrompt
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val abc = BiometricHelper.isBiometricAvailable(this)
-        val s = ""
+        // verifica se pode usar a biometria
+        if (BiometricHelper.isBiometricAvailable(this)) {
+
+            // Thread da aplicação
+            val executor = ContextCompat.getMainExecutor(this)
+            // Logica a ser executada ao interagir com a biomtria
+            val bio = BiometricPrompt(this, executor, object : BiometricPrompt.AuthenticationCallback() {
+                override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
+                    val s = ""
+                    super.onAuthenticationSucceeded(result)
+                }
+            })
+
+            // Dados Parte visual da biometria
+            val info = BiometricPrompt.PromptInfo.Builder()
+                .setTitle("Titulo")
+                .setSubtitle("Sub titulo")
+                .setDescription("descrição")
+                .setNegativeButtonText("Cancelar")
+                .build()
+
+            bio.authenticate(info)
+        }
     }
 }
